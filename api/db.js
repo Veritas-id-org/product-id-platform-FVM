26206b5afc32848d1a85336eccec225d8633d077
Neil Hong <807312043@qq.com>
2023-05-01 13:23:38 +1000

    attemp to use docker

--- api/db.js
+++ api/db.js
@@ -3,12 +3,27 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    // database: "product",
-    authPlugins: {
-        mysql_clear_password: () => Buffer.from(process.env.DB_PASSWORD),
+const dbConfig = {
+    client: "mysql2",
+    connection: {
+        // "db" refers to the name of the service in docker-compose
+        host: "db",
+        // Environment variables are defined in the api service in docker-compose
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        charset: "utf8",
     },
-});
+};
+
+export const db = mysql.createConnection(dbConfig.connection);
+
+// export const db = mysql.createConnection({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//     authPlugins: {
+//         mysql_clear_password: () => Buffer.from(process.env.MYSQL_PASSWORD),
+//     },
+// });
--- api/index.js
+++ api/index.js
@@ -3,11 +3,16 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import { db } from "./db.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
@@ -18,62 +23,59 @@ db.connect((err) => {
         console.log("Database Connection Failed!!!", err);
     } else {
         console.log("Connected to Database");
-        console.log("Creating database");
-        let databaseName = "product";
+        // console.log("Creating database");
+        // let databaseName = "product";
         //Query to create database
-        let databaseQuery = `CREATE DATABASE ${databaseName} `;
-        db.query(databaseQuery, (err, rows) => {
-            if (err) {
-                console.log("Database exits");
-            } else {
-                console.log(`Successfully created database - ${databaseName}`);
-            }
-        });
-        const useDBSql = "USE product";
-        db.query(useDBSql, (err, rows) => {
-            if (err) {
-                throw err;
-            } else {
-                console.log(`Database ${databaseName} is selected`);
-                console.log("Creating database table");
-                let tableName = "users";
-                let tableName2 = "manage_account";
+        // let databaseQuery = `CREATE DATABASE ${databaseName} `;
+        // db.query(databaseQuery, (err, rows) => {
+        //     if (err) {
+        //         console.log("Database exits");
+        //     } else {
+        //         console.log(`Successfully created database - ${databaseName}`);
+        //     }
+        // });
+        // const useDBSql = "USE product";
+        // db.query(useDBSql, (err, rows) => {
+        //     if (err) {
+        //         throw err;
+        //     } else {
+        //         console.log(`Database ${databaseName} is selected`);
 
-                // Query to create table
-                let query = `CREATE TABLE ${tableName} 
+        //     }
+        // });
+        console.log("Creating database table");
+        let tableName = "users";
+        let tableName2 = "manage_account";
+
+        // Query to create table
+        let query = `CREATE TABLE ${tableName} 
 (id INT AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, business_name VARCHAR(255), address VARCHAR(255), website VARCHAR(255), brand_name VARCHAR(255), category VARCHAR(255) 
 )`;
-                // let query = `CREATE TABLE ${tableName}
-                // (id INT AUTO_INCREMENT PRIMARY KEY,
-                // username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, INDEX username_index(username)
-                // )`;
+        // let query = `CREATE TABLE ${tableName}
+        // (id INT AUTO_INCREMENT PRIMARY KEY,
+        // username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, INDEX username_index(username)
+        // )`;
 
-                // let query2 = `CREATE TABLE ${tableName2}
-                // (id INT AUTO_INCREMENT PRIMARY KEY,
-                // name VARCHAR(255) NOT NULL, username VARCHAR(255), address VARCHAR(255) NOT NULL, website VARCHAR(255), CONSTRAINT FK_User_Username FOREIGN KEY(username) REFERENCES users(username)
-                // )`;
+        // let query2 = `CREATE TABLE ${tableName2}
+        // (id INT AUTO_INCREMENT PRIMARY KEY,
+        // name VARCHAR(255) NOT NULL, username VARCHAR(255), address VARCHAR(255) NOT NULL, website VARCHAR(255), CONSTRAINT FK_User_Username FOREIGN KEY(username) REFERENCES users(username)
+        // )`;
 
-                db.query(query, (err, rows) => {
-                    if (err) {
-                        console.log("Table Exist");
-                    } else {
-                        console.log(`Successfully Created Table - ${tableName}`);
-                    }
-                });
+        db.query(query, (err, rows) => {
+            if (err) {
+                console.log("Table Exist");
+            } else {
+                console.log(`Successfully Created Table - ${tableName}`);
             }
         });
-
-        // db.query(query2, (err, rows) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(`Successfully Created Table - ${tableName2}`);
-        //     }
-        // });
     }
 });
 
-app.listen(8800, () => {
+app.get("/", (req, res) => {
+    res.json("Testing Node.js Server");
+});
+
+app.listen(PORT, () => {
     console.log("Server Connected!");
 });
